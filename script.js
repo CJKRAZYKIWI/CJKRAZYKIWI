3ab0abde0dc7dd121b55661e45a5c5b38b25b00e
Cj Galacgac <galacc1@unlv.nevada.edu>
2021-12-18 13:35:55 -0800

    submit button can be pressed

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
+const form = document.querySelector('form');
 
+form.addEventListener('submit', e => {
+  e.preventDefault();
+
+  const values = Array.from(document.querySelectorAll('input[type=checkbox]:checked'))
+    .map(item => item.value)
+    .join(',');
+
+  console.log(`clicked on ?${values}`);
+});
 
 
