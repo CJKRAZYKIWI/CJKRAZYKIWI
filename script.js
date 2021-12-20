a665de96f177da5f4afb29156720c8f71f328221
Cj Galacgac <galacc1@unlv.nevada.edu>
2021-12-20 15:28:09 -0500

    finished

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+
+
 function getSelectedCheckboxValues(name) {
   const checkboxes = document.querySelectorAll(`input[name="${name}"]:checked`);
   let values = [];
@@ -9,5 +11,6 @@ function getSelectedCheckboxValues(name) {
 
 const btn = document.querySelector('#btn');
 btn.addEventListener('click', (event) => {
-  alert("This is correct:"+ getSelectedCheckboxValues('correct'));
-});
+  alert("If you see 6 listed, congrats! Thanks for playing version 1:0:0.: "+ getSelectedCheckboxValues('correct'));
+})
+
